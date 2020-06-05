c13b40fff90ba833d968316264685c7268953459
Erick Duarte <erickduarte@MacBook-Pro.attlocal.net>
2020-06-04 21:17:40 -0500

    updated file

--- Java_T-Mobile/HelloWorld.java
+++ Java_T-Mobile/HelloWorld.java
@@ -1,18 +1,27 @@
 import java.util.Scanner;
+
 public class HelloWorld {
 
     public static void main(String args[]){
         
-        System.out.println("What is your name?");
-        Scanner in = new Scanner(System.in);
-        String name = in.nextLine();
-        System.out.println("Hello "+ name);
-        in.close();
+        // System.out.println("What is your name?");
+        // Scanner in = new Scanner(System.in);
+        // String name = in.nextLine();
+        // System.out.println("Hello "+ name);
+        // in.close();
+        int num = add(2,3);
+        System.out.println(num);
+        System.out.println(isOdd(num));
     }
 
-    public String returningAWord(String word){
-
-        return "hello " + word;
+    public static boolean isOdd( int y) {
+        return y % 2 == 1;
     }
+
+    public static int add(int x , int y){
+        return x+y;
+    }
+
+   
     
 }
