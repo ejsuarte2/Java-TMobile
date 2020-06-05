586bb8b7795ab5d08f8b9bbe808f15b963e784c3
Erick Duarte <erickduarte@MacBook-Pro.attlocal.net>
2020-06-04 20:39:35 -0500

    Added scanner for test

--- Java_T-Mobile/HelloWorld.java
+++ Java_T-Mobile/HelloWorld.java
@@ -1,10 +1,13 @@
-
+import java.util.Scanner;
 public class HelloWorld {
 
     public static void main(String args[]){
-        HelloWorld hw = new HelloWorld();
-
-        System.out.println(hw.returningAWord("word"));
+        
+        System.out.println("What is your name?");
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+        System.out.println("Hello "+ name);
+        in.close();
     }
 
     public String returningAWord(String word){
